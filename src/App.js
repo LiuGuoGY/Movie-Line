f756fac0f6b6428030d3ec46a515d0fe3208e9a8
liuguo <15039006@qq.com>
2022-12-16 16:43:29 +0800

    构建时间线视图

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Timeline } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timeline mode="alternate">
+        <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+        <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+        <Timeline.Item color="red">
+          <p>Solve initial network problems 1</p>
+          <p>Solve initial network problems 2</p>
+          <p>Solve initial network problems 3 2015-09-01</p>
+        </Timeline.Item>
+        <Timeline.Item>
+          <p>Technical testing 1</p>
+          <p>Technical testing 2</p>
+          <p>Technical testing 3 2015-09-01</p>
+        </Timeline.Item>
+        <Timeline.Item color="gray">
+          <p>Technical testing 1</p>
+          <p>Technical testing 2</p>
+          <p>Technical testing 3 2015-09-01</p>
+        </Timeline.Item>
+        <Timeline.Item color="gray">
+          <p>Technical testing 1</p>
+          <p>Technical testing 2</p>
+          <p>Technical testing 3 2015-09-01</p>
+        </Timeline.Item>
+        <Timeline.Item color="#00CCFF" dot={<SmileOutlined />}>
+          <p>Custom color testing</p>
+        </Timeline.Item>
+      </Timeline>
     </div>
   );
 }
