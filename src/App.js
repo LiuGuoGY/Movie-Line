7aa856d56af153927b625ea811e91410bde9b60d
liuguo <15039006@qq.com>
2022-12-19 10:28:14 +0800

    增加左上角年份显示

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { Timeline } from 'antd';
-import { SmileOutlined } from '@ant-design/icons';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <Timeline mode="alternate">
-        <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
-        <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
-        <Timeline.Item color="red">
-          <p>Solve initial network problems 1</p>
-          <p>Solve initial network problems 2</p>
-          <p>Solve initial network problems 3 2015-09-01</p>
-        </Timeline.Item>
-        <Timeline.Item>
-          <p>Technical testing 1</p>
-          <p>Technical testing 2</p>
-          <p>Technical testing 3 2015-09-01</p>
-        </Timeline.Item>
-        <Timeline.Item color="gray">
-          <p>Technical testing 1</p>
-          <p>Technical testing 2</p>
-          <p>Technical testing 3 2015-09-01</p>
-        </Timeline.Item>
-        <Timeline.Item color="gray">
-          <p>Technical testing 1</p>
-          <p>Technical testing 2</p>
-          <p>Technical testing 3 2015-09-01</p>
-        </Timeline.Item>
-        <Timeline.Item color="#00CCFF" dot={<SmileOutlined />}>
-          <p>Custom color testing</p>
-        </Timeline.Item>
-      </Timeline>
+    <div className={styles.App}>
+      <div className={styles.appBackgroud}>
+        <div className={styles.ellipse1} />
+        <div className={styles.ellipse2} />
+        <div className={styles.ellipse3} />
+        <div className={styles.ellipse4} />
+        <div className={styles.ellipse5} />
+        <div className={styles.ellipse6} />
+        <div className={styles.ellipse7} />
+      </div>
+      <div className={styles.dateBox}>
+        <p>2022</p>
+        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M8 10L10.2929 12.2929C10.6834 12.6834 11.3166 12.6834 11.7071 12.2929L14 10M1 16L1 6C1 3.23858 3.23858 1 6 1L16 1C18.7614 1 21 3.23858 21 6V16C21 18.7614 18.7614 21 16 21H6C3.23858 21 1 18.7614 1 16Z" stroke="black" stroke-linecap="round" />
+        </svg>
+      </div>
     </div>
   );
 }
